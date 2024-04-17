7a3fadc9eea01cb583bf55691457d06065edb76e
Peter Istvan <peter.istvan@edu.bme.hu>
2024-04-17 11:25:30 +0200

    Added basic train script

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 from tqdm import tqdm
+# from torch_geometric.nn import summary
 
 from baseline_model_fusion import BaselineFusionGNN
 
@@ -14,12 +15,23 @@ if __name__ == "__main__":
     opt = torch.optim.Adam(model.parameters())
     loss_fn = torch.nn.MSELoss()
 
+    print(model)
+
     dataloader = torch.load(f"{task}_{split}_dataloader.pt")
-    for mesh, connectome, y in (pbar := tqdm(dataloader)):
-        opt.zero_grad()
-        y_pred = model(mesh, connectome).squeeze()
-        loss = loss_fn(y_pred, y)
-        loss.backward()
-        opt.step()
-        pbar.update(f"Loss: {loss.detach().item()}")
+    for epoch in range(10):
+        loss_total = 0.0
+        samples_total = 0.0
+
+        for mesh, connectome, y in (pbar := tqdm(dataloader)):
+            opt.zero_grad()
+            y_pred = model(mesh, connectome).squeeze()
+            loss = loss_fn(y_pred, y)
+            loss.backward()
+            opt.step()
+
+            loss_total += loss.detach().item()
+            samples_total += mesh.num_graphs # 'Batch'
+            # pbar.update(f"Loss: {loss.detach().item():.4f}")
+
+        print(f"Avg. MSE: {loss_total / samples_total: .4f}")
         
