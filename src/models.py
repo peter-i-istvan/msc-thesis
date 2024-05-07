1bb374def77c3fe48b9f8be37f7a4151640d3e93
Peter Istvan <peter.istvan@edu.bme.hu>
2024-05-07 18:27:00 +0200

    Restructured model handling

---
+++ src/models.py
@@ -0,0 +1,183 @@
+import torch
+from dataclasses import dataclass
+from torch_geometric.nn.dense import DenseGraphConv
+from torch_geometric.nn import GCNConv, global_mean_pool
+from torch.nn import Module, BatchNorm1d, ReLU, PReLU, Flatten, Linear, Sequential
+
+
+@dataclass(frozen=True)
+class ConnectomeConf:
+    NODES = 87 # assumed to be fixed
+    in_channel = 10
+    conv_channel = 20
+    conv_out = 5
+    agg = "flatten" # "flatten" | "add"
+
+    @property
+    def out(self):
+        if "flatten":
+            return self.NODES * self.conv_out
+        else: # add
+            return self.conv_out
+
+
+@dataclass(frozen=True)
+class MeshConf:
+    in_channel = 13
+    hidden = 32
+    
+    @property
+    def out(self):
+        return self.hidden
+
+
+@dataclass(frozen=True)
+class HeadConf:
+    in_agg = "cat" # "cat" | "add" | None -> only relevant for FusionGNN
+    hidden = 10 # conn: 10, mesh: 32
+
+
+class MeshFeatureExtractor(Module):
+    """Returns features after mean pool."""
+
+    def __init__(self, conf: MeshConf):
+        super().__init__()
+        self.bn0 = BatchNorm1d(conf.in_channel)
+        self.fc1 = GCNConv(conf.in_channel, conf.hidden)
+        self.bn1 = BatchNorm1d(conf.hidden)
+        self.fc2 = GCNConv(conf.hidden, conf.hidden)
+        self.bn2 = BatchNorm1d(conf.hidden)
+        self.fc3 = GCNConv(conf.hidden, conf.hidden)
+        self.bn3 = BatchNorm1d(conf.hidden)
+        self.fc4 = GCNConv(conf.hidden, conf.hidden)
+        self.bn4 = BatchNorm1d(conf.hidden)
+        self.act = ReLU()
+
+    def forward(self, mesh):
+        # assemble inputs based on available features
+        x = mesh.pos
+        if mesh.norm is not None:
+            x = torch.cat([x, mesh.norm], dim=1)
+        if mesh.dha is not None:
+            x = torch.cat([x, mesh.dha], dim=1)
+        if mesh.x is not None:
+            x = torch.cat([x, mesh.x], dim=1)
+        
+        # run feature extractor
+        x = self.bn0(x)
+        x = self.bn1(self.act(self.fc1(x, mesh.edge_index)))
+        x = self.bn2(self.act(self.fc2(x, mesh.edge_index)))
+        x = self.bn3(self.act(self.fc3(x, mesh.edge_index)))
+        x = self.bn4(self.act(self.fc4(x, mesh.edge_index)))
+        x = global_mean_pool(x, mesh.batch)
+
+        return x
+    
+
+class ConnectomeFeatureExtractor(Module):
+    """Returns features after aggregation (sum or concat)."""
+    def __init__(self, conf: ConnectomeConf):
+        super().__init__()
+        self.conv1 = DenseGraphConv(conf.in_channel, conf.conv_channel)
+        self.prelu1 = PReLU()
+        self.conv2 = DenseGraphConv(conf.conv_channel, conf.conv_channel)
+        self.prelu2 = PReLU()
+        self.conv3 = DenseGraphConv(conf.conv_channel, conf.conv_out)
+        self.prelu3 = PReLU()
+        
+        if conf.agg == "flatten":
+            self.flatten = Flatten()
+        else:
+            self.flatten = None
+
+    def forward(self, connectome):
+        x, adj = connectome.x, connectome.adj
+        x = self.prelu1(self.conv1(x, adj))
+        x = self.prelu2(self.conv2(x, adj))
+        x = self.prelu3(self.conv3(x, adj))
+        # final aggregation
+        if self.flatten is not None:
+            x = self.flatten(x)
+        else:
+            x = torch.sum(x, dim=-1)
+
+        return x
+    
+
+class MeshGNN(Module):
+    def __init__(self, mesh_conf: MeshConf, head_conf: HeadConf) -> None:
+        super().__init__()
+        self.feature_extractor = MeshFeatureExtractor(mesh_conf)
+
+        self.head = Sequential([
+            Linear(mesh_conf.out, head_conf.hidden),
+            ReLU(),
+            BatchNorm1d(head_conf.hidden),
+            Linear(head_conf.hidden, 1)
+        ])
+
+    def forward(self, mesh, connectome):
+        """Ignore connectome, but keep it for uniform interface."""
+        features = self.feature_extractor(mesh)
+        return self.head(features)
+
+
+class ConnectomeGNN(Module):
+    def __init__(self, connectome_conf: ConnectomeConf, head_conf: HeadConf) -> None:
+        super().__init__()
+        self.feature_extractor = ConnectomeFeatureExtractor(connectome_conf)
+
+        self.head = Sequential([
+            Linear(connectome_conf.out, head_conf.hidden),
+            ReLU(),
+            BatchNorm1d(head_conf.hidden),
+            Linear(head_conf.hidden, 1)
+        ])
+
+    def forward(self, mesh, connectome):
+        """Ignore connectome, but keep it for uniform interface."""
+        features = self.feature_extractor(connectome)
+        return self.head(features)
+    
+
+class FusionGNN(Module):
+    def __init__(
+        self,
+        connectome_conf: ConnectomeConf,
+        mesh_conf: MeshConf,
+        head_conf: HeadConf
+    ) -> None:
+        
+        super().__init__()
+
+        self.mesh = MeshFeatureExtractor(mesh_conf)
+        self.connectome = ConnectomeFeatureExtractor(connectome_conf)
+
+        # Handle aggregation
+        assert head_conf.in_agg in {"add", "cat"}
+        self.agg = head_conf.in_agg
+
+        if head_conf.in_agg == "add":
+            msg = "Mesh and Connectome feature outputs must match if we aggregate by addition"
+            assert mesh_conf.out == connectome_conf.out, msg
+            head_in = mesh_conf.out
+        else:
+            head_in = mesh_conf.out + connectome_conf.out
+
+        self.head = Sequential([
+            Linear(head_in, head_conf.hidden),
+            ReLU(),
+            BatchNorm1d(head_conf.hidden),
+            Linear(head_conf.hidden, 1)
+        ])
+
+    def forward(self, mesh, connectome):
+        connectome_features = self.connectome(connectome)
+        mesh_features = self.mesh(mesh)
+        
+        if self.agg == "add":
+            features = connectome_features + mesh_features
+        else:
+            features = torch.cat([mesh_features, connectome_features], dim=1)
+
+        return self.head(features)
