09166eec4491b9f1dc4c5ecb2139a7ebd76cb778
Péter István <peter.istvan@edu.bme.hu>
2024-08-30 00:05:48 +0200

    Fixed basic explainer train code

--- explain.py
+++ explain.py
@@ -1,13 +1,15 @@
 import torch
 import random
-from torch.optim import Adam
+from tqdm import tqdm
+from torch.optim import Adam, SGD
 from lightning import seed_everything
 from torch.nn import Module, BatchNorm1d, ReLU, Linear, Sequential, MSELoss
-from models import MeshGNN, MeshConf, HeadConf, concrete_sample, apply_mask
+from src.models import MeshGNN, MeshConf, HeadConf, concrete_sample, apply_mask
 
 
-random.seed(0)
 seed_everything(0)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 
 
 class PGExplainer(Module):
@@ -25,7 +27,7 @@ class PGExplainer(Module):
             ReLU(),
             BatchNorm1d(head_hidden),
             Linear(head_hidden, 1)
-        )
+        ).to(device)
 
     def forward(self, features):
         return self.head(features)
@@ -33,21 +35,24 @@ class PGExplainer(Module):
     def fit(self, epochs, model, dataloader):
         optimizer = Adam(model.parameters(), lr=1e-3)
 
-        for _ in range(epochs):
-            for mesh in dataloader:
+        for _ in tqdm(range(epochs), desc='Fitting explainer'):
+            acc_loss = 0
+            for mesh, _, y in dataloader:
                 optimizer.zero_grad()
 
-                z = model.feature_extractor(mesh, pool=False)
+                z = model.feature_extractor(mesh.to(device), pool=False)
                 z = self(z)
                 mask = concrete_sample(z, temperature=2.0)
-                data = apply_mask(mesh, mask)
-                y_pred = model(data)
+                masked_mesh = apply_mask(mesh, mask)
+                y_pred = model(masked_mesh, None)
 
-                loss = self._loss(y_pred, mesh.y, mask)
+                loss = self._loss(y_pred, y.to(device), mask)
                 loss.backward()
                 optimizer.step()
 
-                print(loss)
+                acc_loss += loss.cpu().detach().item()
+
+            print(acc_loss / len(dataloader))
 
     def _loss(self, y_hat: torch.Tensor, y: torch.Tensor, node_mask: torch.Tensor) -> torch.Tensor:
         loss = self.target_loss(y_hat, y)
@@ -69,34 +74,37 @@ def evaluate(model, dataloader, loss_fn, split):
     with torch.no_grad():
         for mesh, _, y in dataloader:
             random_bit = random.random() > 0.5
-            y_pred = model(mesh, None, mask=random_bit)
-            loss = loss_fn(y, y_pred)
+            y_pred = model(mesh.to(device), None, mask=random_bit).squeeze()
+            loss = loss_fn(y.to(device), y_pred)
             loss_acc += loss.cpu().detach().item()
             n_graphs += mesh.num_graphs
 
-    print(f"Avg {split} MSE: {loss_acc / n_graphs}")
+    avg_loss = loss_acc / n_graphs
+    print(f"Avg {split} MSE: {avg_loss}")
+    return avg_loss
 
 
 def train_with_random() -> MeshGNN:
     # train on birth age with random masking:
-    model = MeshGNN(mesh_conf=MeshConf(), head_conf=HeadConf(hidden=32))
-    optimizer = Adam(model.parameters(), lr=1e-3)
+    model = MeshGNN(mesh_conf=MeshConf(), head_conf=HeadConf(hidden=32)).to(device)
+    optimizer = Adam(model.parameters(), lr=5e-4)
     loss_fn = MSELoss()
 
-    train_dataloader = torch.load("birth_age_train_dataloader.pt")
-    # val_dataloader = torch.load("birth_age_val_dataloader.pt")
-    # test_dataloader = torch.load("birth_age_test_dataloader.pt")
+    train_dataloader = torch.load("data/birth_age/train/birth_age_train_dataloader.pt")
+    val_dataloader = torch.load("data/birth_age/val/birth_age_val_dataloader.pt")
+    test_dataloader = torch.load("data/birth_age/test/birth_age_test_dataloader.pt")
 
-    for epoch in range(200):
+    for _ in tqdm(range(50)):
         loss_acc, n_graphs = 0, 0
 
         model.train()
-        for mesh, _, y in train_dataloader:
+        for batch in train_dataloader:
+            mesh, _, y = batch
             optimizer.zero_grad()
 
-            random_bit = random.random() > 0.5
-            y_pred = model(mesh, None, mask=random_bit)
-            loss = loss_fn(y, y_pred)
+            random_bit = random.random() > 0.75
+            y_pred = model(mesh.to(device), None, mask=random_bit).squeeze()
+            loss = loss_fn(y.to(device), y_pred)
 
             loss.backward()
             optimizer.step()
@@ -106,28 +114,29 @@ def train_with_random() -> MeshGNN:
 
         print(f"Avg Train MSE: {loss_acc / n_graphs}")
 
-        # evaluate(model, val_dataloader, loss_fn, "Val")
+        evaluate(model, val_dataloader, loss_fn, "Val")
 
-    # evaluate(model, test_dataloader, loss_fn, "Test")
+    evaluate(model, test_dataloader, loss_fn, "Test")
     torch.save(model, "model_random.pt")
 
     return model
 
 
 def explain(model: MeshGNN):
-    train_dataloader = torch.load("birth_age_train_dataloader.pt")
-    test_dataloader = torch.load("birth_age_test_dataloader.pt")
+    # dataloader = torch.load("birth_age_train_dataloader.pt")
+    dataloader = torch.load("data/birth_age/test/birth_age_test_dataloader.pt")
 
     # freeze feature extractor
     for param in model.feature_extractor.parameters():
         param.requires_grad = False
 
     explainer = PGExplainer()
-    explainer.fit(epochs=50, model=model, dataloader=test_dataloader)
+    explainer.fit(epochs=50, model=model, dataloader=dataloader)
 
 
 def main():
-    model = train_with_random()
+    # model = train_with_random()
+    model = torch.load("models/model.pt").model.to(device)
     explain(model)
 
 
